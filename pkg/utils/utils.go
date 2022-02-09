b39ba3e3ef7c74810ae57c9a76c02f07772d757e
Filipe Alves <43504172+felipeagger@users.noreply.github.com>
2022-02-09 14:10:01 -0300

    Updating utils.NodeID

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,6 +3,9 @@ package utils
 import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/bwmarrin/snowflake"
 )
@@ -12,7 +15,7 @@ var snowflk *snowflake.Node
 func init() {
 
 	var err error
-	snowflk, err = snowflake.NewNode(1)
+	snowflk, err = snowflake.NewNode(GenerateNodeID())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -31,4 +34,25 @@ func GenerateHashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func GenerateNodeID() int64 {
+	rand.Seed(time.Now().UnixNano())
+
+	name, _ := os.Hostname()
+	if name == "" {
+		return int64(rand.Intn(1023))
+	}
+
+	id  := int64(rand.Intn(100))
+	for _, letter := range []byte(name) {
+
+		if id + int64(letter) >= 1023 {
+			return id
+		} else {
+			id = id + int64(letter)
+		}
+	}
+
+	return id
 }
