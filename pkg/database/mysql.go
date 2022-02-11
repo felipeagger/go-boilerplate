47b7be632bc352ae4120269053577f1151f7978a
Filipe Alves <43504172+felipeagger@users.noreply.github.com>
2022-02-11 11:31:05 -0300

    adjusting database connection params.

--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -2,19 +2,15 @@ package database
 
 import (
 	"fmt"
-
 	gormotel "github.com/wei840222/gorm-otel"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
 //NewMySQLConnection create a new db connection
 func NewMySQLConnection(dbHost, dbName, dbUser, dbPass string) (db *gorm.DB, err error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
-
-	//db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
-	//db.SetMaxIdleConns(cfg.MaxIdleConns)
-	//db.SetMaxOpenConns(cfg.MaxOpenConns)
 
     db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -22,5 +18,18 @@ func NewMySQLConnection(dbHost, dbName, dbUser, dbPass string) (db *gorm.DB, err
 	}
 
 	err = db.Use(gormotel.New())
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+
 	return db, err
 }
